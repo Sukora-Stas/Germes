4cbaa6696c279845ceed4f88f4dadba3890e3808
SukoraStas <stasiksukora@mail.ru>
2017-09-21 13:54:47 +0300

    germes: add log4j version 0.1.4

--- germes-model/src/main/java/org/itsimulator/germes/app/infra/util/CommonUtil.java
+++ germes-model/src/main/java/org/itsimulator/germes/app/infra/util/CommonUtil.java
@@ -1,5 +1,8 @@
 package org.itsimulator.germes.app.infra.util;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -18,22 +21,37 @@ public class CommonUtil {
 
     /**
      * Returns not-null unmodifiable copy of the source set
+     *
      * @param source
      * @return
      */
     public static <T> Set<T> getSafeSet(Set<T> source) {
-        return Collections.unmodifiableSet(Optional.ofNullable(source).orElse(Collections.emptySet()));
+        return Collections.unmodifiableSet(Optional.ofNullable(source).orElse(
+                Collections.emptySet()));
     }
 
     /**
      * Returns not-null unmodifiable copy of the source list
+     *
      * @param source
      * @return
      */
     public static <T> List<T> getSafeList(List<T> source) {
-        return Collections.unmodifiableList(Optional.ofNullable(source).orElse(Collections.emptyList()));
+        return Collections.unmodifiableList(Optional.ofNullable(source).orElse(
+                Collections.emptyList()));
     }
 
+    /**
+     * Dynamically converts param into string representation using all
+     * object state
+     *
+     * @param param
+     * @return
+     */
+    public static String toString(Object param) {
+        return ReflectionToStringBuilder.toString(param,
+                ToStringStyle.SHORT_PREFIX_STYLE);
+    }
 }
 
 
