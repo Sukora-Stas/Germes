77fc4abdfaa23712205f644e475a7857db1d95a3
SukoraStas <stasiksukora@mail.ru>
2017-09-21 14:39:49 +0300

    germes: GlobalExceptionHandler version 0.1.8

---
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package org.itsimulator.germes.app.rest.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Created by Sukora Stas.
+ */
+@Provider
+public class GlobalExceptionHandler implements ExceptionMapper<Exception> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private final Response SERVER_ERROR;
+
+    public GlobalExceptionHandler() {
+        SERVER_ERROR = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @Override
+    public Response toResponse(Exception ex) {
+        LOGGER.error(ex.getMessage(), ex);
+
+        return SERVER_ERROR;
+    }
+}
---
+++ germes-service/src/test/java/org/itsimulator/germes/app/rest/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,34 @@
+package org.itsimulator.germes.app.rest.exception;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sukora Stas.
+ * Verifies that {@link GlobalExceptionHandler} correctly handles application
+ * exceptions
+ */
+public class GlobalExceptionHandlerTest {
+    /**
+     * Current exception handler
+     */
+    private ExceptionMapper<Exception> handler;
+
+    @Before
+    public void setup() {
+        handler = new GlobalExceptionHandler();
+    }
+
+    @Test
+    public void testToResponseReturnServerError() {
+        Exception exception = new Exception("test");
+        Response response = handler.toResponse(exception);
+        assertEquals(response.getStatus(), Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    }
+
+}
