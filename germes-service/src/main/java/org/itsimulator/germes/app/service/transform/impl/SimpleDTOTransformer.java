647b7837c24eea5f624f21ae65fcf0edb91fbabb
SukoraStas <stasiksukora@mail.ru>
2017-09-21 13:42:48 +0300

    germes: add log4j version 0.1.3

--- germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
@@ -5,19 +5,22 @@ package org.itsimulator.germes.app.service.transform.impl;
  */
 
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.itsimulator.germes.app.infra.util.Checks;
 import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
 import org.itsimulator.germes.app.rest.dto.base.BaseDTO;
 import org.itsimulator.germes.app.service.transform.Transformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Default transformation engine that uses reflection to transform objects
- *
- * @author Morenets
- *
  */
 public class SimpleDTOTransformer implements Transformer {
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(SimpleDTOTransformer.class);
 
     @Override
     public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(
@@ -30,11 +33,18 @@ public class SimpleDTOTransformer implements Transformer {
                 ReflectionUtil.findSimilarFields(entity.getClass(), clz));
         dto.transform(entity);
 
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.transform: {} DTO object",
+                    ReflectionToStringBuilder.toString(dto,
+                            ToStringStyle.SHORT_PREFIX_STYLE));
+        }
+
         return dto;
     }
 
     private void checkParams(final Object param, final Class<?> clz) {
-        Checks.checkParameter(param != null, "Source transformation object is not initialized");
+        Checks.checkParameter(param != null,
+                "Source transformation object is not initialized");
         Checks.checkParameter(clz != null,
                 "No class is defined for transformation");
     }
@@ -50,6 +60,13 @@ public class SimpleDTOTransformer implements Transformer {
                 ReflectionUtil.findSimilarFields(dto.getClass(), clz));
         dto.untransform(entity);
 
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.transform: {} entity",
+                    ReflectionToStringBuilder.toString(entity,
+                            ToStringStyle.SHORT_PREFIX_STYLE));
+        }
+
         return entity;
     }
 }
+
