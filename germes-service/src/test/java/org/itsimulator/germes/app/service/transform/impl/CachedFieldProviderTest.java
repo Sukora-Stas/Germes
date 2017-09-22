d7cfbebd847558e0a8909a5fc739010f0119b13a
SukoraStas <stasiksukora@mail.ru>
2017-09-22 15:35:13 +0300

    germes: JMockit version 0.2.2

--- germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CachedFieldProviderTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CachedFieldProviderTest.java
@@ -2,16 +2,26 @@ package org.itsimulator.germes.app.service.transform.impl;
 
 import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
 
 import java.util.Collections;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by Sukora Stas.
@@ -23,10 +33,15 @@ import static org.mockito.Mockito.*;
  * Verifies functionality of the {@link SimpleDTOTransformer}
  * unit
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(ReflectionUtil.class)
+
+// @RunWith(PowerMockRunner.class)
+// @PrepareForTest(ReflectionUtil.class)
+@RunWith(JMockit.class)
 public class CachedFieldProviderTest {
     private FieldProvider provider;
+
+    //@Mocked
+    //private final ReflectionUtil reflectionUtil = null;
 
     @Before
     public void setup() {
@@ -49,11 +64,62 @@ public class CachedFieldProviderTest {
     }
 
     @Test
+    @Ignore
     public void testGetFieldNamesAreCached() {
         List<String> fields = provider.getFieldNames(Source.class, Destination.class);
 
         PowerMockito.mockStatic(ReflectionUtil.class);
         when(ReflectionUtil.findSimilarFields(Source.class, Destination.class)).thenReturn(Collections.emptyList());
+
+        assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
+        List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+        assertFalse(fields.isEmpty());
+        assertEquals(fields, fields2);
+    }
+
+    @Test
+    @Ignore
+    public void testGetFieldNamesAreCachedUsingJMockit() {
+        new Expectations() {
+            {
+                ReflectionUtil.findSimilarFields(Source.class, Destination.class);
+                result = Collections.singletonList("name");
+            }
+        };
+
+        List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+
+        new Expectations() {
+            {
+                ReflectionUtil.findSimilarFields(Source.class, Destination.class);
+                result = Collections.emptyList();
+            }
+        };
+
+        assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
+        List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+        assertFalse(fields.isEmpty());
+        assertEquals(fields, fields2);
+    }
+
+    public static final class MockReflectionUtil extends MockUp<ReflectionUtil> {
+        protected static List<String> fields;
+
+        @Mock
+        public static List<String> findSimilarFields(Class<?> clz1, Class<?> clz2) {
+            return fields;
+        }
+    }
+
+    @Test
+    public void testGetFieldNamesAreCachedUsingMockupsAPI() {
+        new MockReflectionUtil();
+
+        MockReflectionUtil.fields = Collections.singletonList("name");
+
+        List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+
+        MockReflectionUtil.fields = Collections.emptyList();
 
         assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
         List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
