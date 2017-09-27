467f1105e9b36f358e2d349388b9f253f3d9e76b
SukoraStas <stasiksukora@mail.ru>
2017-09-27 15:44:04 +0300

    germes: add test version 0.4.1

--- germes-service/src/test/java/org/itsimulator/germes/app/service/GeographicServiceImplTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/service/GeographicServiceImplTest.java
@@ -1,7 +1,11 @@
 package org.itsimulator.germes.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.itsimulator.germes.app.model.entity.geography.City;
 import org.itsimulator.germes.app.model.entity.geography.Station;
@@ -15,7 +19,9 @@ import org.itsimulator.germes.app.persistence.repository.hibernate.HibernateCity
 import org.itsimulator.germes.app.persistence.repository.hibernate.HibernateStationRepository;
 import org.itsimulator.germes.app.persistence.repository.inmemory.InMemoryCityRepository;
 import org.itsimulator.germes.app.service.impl.GeographicServiceImpl;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -26,14 +32,23 @@ import static org.junit.Assert.*;
 public class GeographicServiceImplTest {
     private static final int DEFAULT_CITY_ID = 1;
 
-    private GeographicService service;
+    private static GeographicService service;
 
-    @Before
-    public void setup() {
+    private static ExecutorService executorService;
+
+    @BeforeClass
+    public static void setup() {
         SessionFactoryBuilder builder = new SessionFactoryBuilder();
         CityRepository repository = new HibernateCityRepository(builder);
         StationRepository stationRepository = new HibernateStationRepository(builder);
         service = new GeographicServiceImpl(repository, stationRepository);
+
+        executorService = Executors.newCachedThreadPool();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        executorService.shutdownNow();
     }
 
     @Test
@@ -44,11 +59,13 @@ public class GeographicServiceImplTest {
 
     @Test
     public void testSaveNewCitySuccess() {
+        int cityCount = service.findCities().size();
+
         City city = createCity();
         service.saveCity(city);
 
         List<City> cities = service.findCities();
-        assertEquals(cities.size(), 5);
+        assertEquals(cities.size(), cityCount + 1);
         assertEquals(cities.get(0).getName(), "Odessa");
     }
 
@@ -90,6 +107,9 @@ public class GeographicServiceImplTest {
 
     @Test
     public void testSearchStationsByTransportTypeSuccess() {
+        int stationCount = service.searchStations(new StationCriteria(TransportType.AUTO), new RangeCriteria(1, 5))
+                .size();
+
         City city = createCity();
         city.addStation(TransportType.AUTO);
         service.saveCity(city);
@@ -99,9 +119,10 @@ public class GeographicServiceImplTest {
         city2.addStation(TransportType.AUTO);
         service.saveCity(city2);
 
-        List<Station> stations = service.searchStations(new StationCriteria(TransportType.AUTO), new RangeCriteria(1, 5));
+        List<Station> stations = service.searchStations(new StationCriteria(TransportType.AUTO),
+                new RangeCriteria(1, 5));
         assertNotNull(stations);
-        assertEquals(stations.size(), 3);
+        assertEquals(stations.size(), stationCount + 2);
     }
 
     @Test
@@ -114,11 +135,92 @@ public class GeographicServiceImplTest {
         city2.addStation(TransportType.RAILWAY);
         service.saveCity(city2);
 
-        List<Station> stations = service.searchStations(new StationCriteria(TransportType.AVIA), new RangeCriteria(1, 5));
+        List<Station> stations = service.searchStations(new StationCriteria(TransportType.AVIA),
+                new RangeCriteria(1, 5));
         assertNotNull(stations);
         assertTrue(stations.isEmpty());
     }
 
+    @Test
+    public void testSaveMultipleCitiesSuccess() {
+        int cityCount = service.findCities().size();
+
+        int addedCount = 100_000;
+        for (int i = 0; i < addedCount; i++) {
+            City city = new City("Odessa" + i);
+            city.setDistrict("Odessa");
+            city.setRegion("Odessa");
+            city.addStation(TransportType.AUTO);
+            service.saveCity(city);
+        }
+
+        List<City> cities = service.findCities();
+        assertEquals(cities.size(), cityCount + addedCount);
+    }
+
+    @Test
+    public void testSaveMultipleCitiesConcurrentlySuccess() {
+        int cityCount = service.findCities().size();
+
+        int threadCount = 200;
+        int batchCount = 10;
+
+        List<Future<?>> futures = new ArrayList<>();
+
+        for (int i = 0; i < threadCount; i++) {
+            futures.add(executorService.submit(() -> {
+                for (int j = 0; j < batchCount; j++) {
+                    City city = new City("Lviv_" + Math.random());
+                    city.setDistrict("Lviv");
+                    city.setRegion("Lviv");
+                    city.addStation(TransportType.AUTO);
+                    service.saveCity(city);
+                }
+            }));
+        }
+
+        waitForFutures(futures);
+
+        List<City> cities = service.findCities();
+        assertEquals(cities.size(), cityCount + threadCount * batchCount);
+    }
+
+    @Test
+    public void testSaveOneCityConcurrentlySuccess() {
+        City city = new City("Nikolaev");
+        city.setDistrict("Nikolaev");
+        city.setRegion("Nikolaev");
+        city.addStation(TransportType.AUTO);
+        service.saveCity(city);
+
+        int cityCount = service.findCities().size();
+
+        int threadCount = 200;
+
+        List<Future<?>> futures = new ArrayList<>();
+
+        for (int i = 0; i < threadCount; i++) {
+            futures.add(executorService.submit(() -> {
+                city.setName("Nikolaev" + Math.random());
+                service.saveCity(city);
+            }));
+        }
+
+        waitForFutures(futures);
+
+        List<City> cities = service.findCities();
+        assertEquals(cities.size(), cityCount);
+    }
+
+    private void waitForFutures(List<Future<?>> futures) {
+        futures.forEach(future -> {
+            try {
+                future.get();
+            } catch (Exception e) {
+                fail(e.getMessage());
+            }
+        });
+    }
 
     private City createCity() {
         City city = new City("Odessa");
