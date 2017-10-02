b483ec5a5d3f6ac7ec46888a415722bf5cd38bfb
SukoraStas <stasiksukora@mail.ru>
2017-10-02 13:46:19 +0300

    germes: use angular-translate, refactoring version 0.6.7

--- germes-presentation/src/main/webapp/js/app.js
+++ germes-presentation/src/main/webapp/js/app.js
@@ -1,32 +1,16 @@
 'use strict';
 
-var app = angular.module('app', ['ngResource', 'pascalprecht.translate']);
+var app = angular.module('app', [ 'ngResource', 'pascalprecht.translate' ]);
 
-app.factory('cityService', ['$resource', function ($resource) {
+app.factory('cityService', [ '$resource', function($resource) {
     return $resource('/Germes/api/cities');
-}]);
+} ]);
 
-app.config(function ($translateProvider) {
-    $translateProvider.translations('en', {
-        TITLE: 'Germes project',
-        CITIES: 'Cities',
-        CITY_NAME: 'Name',
-        CITY_DISTRICT: 'District',
-        CITY_REGION: 'Region'
-    });
-    $translateProvider.translations('ru', {
-        TITLE: 'Проект Гермес',
-        CITIES: 'Населенные пункты',
-        CITY_NAME: 'Название',
-        CITY_DISTRICT: 'Район',
-        CITY_REGION: 'Область'
-    });
-    $translateProvider.translations('uk', {
-        TITLE: 'Проект Гермес',
-        CITIES: 'Населені пункти',
-        CITY_NAME: 'Назва',
-        CITY_DISTRICT: 'Район',
-        CITY_REGION: 'Область'
+app.config(function($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'l10n/locale-',
+        suffix: '.json'
     });
     $translateProvider.preferredLanguage('en');
+    $translateProvider.useSanitizeValueStrategy('escape');
 });
