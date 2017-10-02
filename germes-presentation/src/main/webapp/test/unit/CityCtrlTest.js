d9eb9295b581a28bc43058192464c7ab25ad7dd8
SukoraStas <stasiksukora@mail.ru>
2017-10-02 12:56:34 +0300

    germes: add Front-End tests version 0.6.3

--- germes-presentation/src/main/webapp/test/unit/CityCtrlTest.js
+++ germes-presentation/src/main/webapp/test/unit/CityCtrlTest.js
@@ -1,43 +1,61 @@
-describe('Testing City Controller', function() {
+describe('Testing City Controller', function () {
 
     beforeEach(module("app"));
 
-    var scope = {};
-    beforeEach(inject(function($controller) {
-        $controller('CityCtrl', { $scope : scope});
+    var scope = {}, httpBackend;
+    beforeEach(inject(function ($controller, $httpBackend) {
+        $controller('CityCtrl', {$scope: scope});
+        httpBackend = $httpBackend;
     }));
 
-    afterEach(function() {
+    afterEach(function () {
         //Cleaning resources
     });
 
-    it('Rows per page should have initial value', function() {
+    it('Rows per page should have initial value', function () {
 
         expect(scope.rowsPerPage).toBeDefined();
         expect(scope.rowsPerPage).toBe(10);
     });
 
-    it('Should return false for non-region center', function() {
+    it('Should return false for non-region center', function () {
 
         var city = {
-            name : 'Gribovka',
-            district : 'Ovidiopol',
-            region : 'Odessa'
+            name: 'Gribovka',
+            district: 'Ovidiopol',
+            region: 'Odessa'
         };
         var center = scope.isRegionCenter(city);
 
         expect(center).toBeFalsy();
     });
 
-    it('Should return true for region center', function() {
+    it('Should return true for region center', function () {
 
         var city = {
-            name : 'Kyiv',
-            district : '',
-            region : 'Kyiv'
+            name: 'Kyiv',
+            district: '',
+            region: 'Kyiv'
         };
         var center = scope.isRegionCenter(city);
 
         expect(center).toBeTruthy();
+    });
+
+    it('Should query cities', function () {
+
+        httpBackend.expectGET("/api/cities").respond(
+            [
+                {
+                    'name': 'Odessa',
+                    'district': '',
+                    'region': 'Odessa'
+                }
+            ]);
+        httpBackend.flush();
+
+        expect(scope.cities[0].name).toBe('Odessa');
+        expect(scope.cities[0].district).toBe('');
+        expect(scope.cities[0].region).toBe('Odessa');
     });
 });
