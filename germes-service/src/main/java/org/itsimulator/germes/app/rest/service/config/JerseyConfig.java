0d956d43b267ae5268d1a5e3fcf34819d684faf5
SukoraStas <stasiksukora@mail.ru>
2017-10-06 12:55:48 +0300

    germes: add API documentation version 0.7.2

--- germes-service/src/main/java/org/itsimulator/germes/app/rest/service/config/JerseyConfig.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/service/config/JerseyConfig.java
@@ -1,5 +1,8 @@
 package org.itsimulator.germes.app.rest.service.config;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.itsimulator.germes.app.config.ComponentFeature;
 
@@ -17,5 +20,18 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         super(ComponentFeature.class);
         packages("org.itsimulator.germes.app.rest");
+
+        register(ApiListingResource.class);
+        register(SwaggerSerializers.class);
+    }
+
+    private void initBeanConfig() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setSchemes(new String[] { "http" });
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("org.itsimulator.germes.app.rest.service");
+        beanConfig.setScan(true);
     }
 }
